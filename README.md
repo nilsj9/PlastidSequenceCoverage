b4da8175e57c8d4e647b53af3ba3673897345718
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2021-05-04 21:43:09 +0200

    general repo structure

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# PlastidSequenceCoverage
+*Pipeline for Inferring Plastid Sequencing Depth and Evenness*
+==============================================================
+
+A bioinformatic pipeline for inferring sequencing depth and evenness on complete plastid genomes
+
+
+Description of Pipeline
+-----------------------
+
+This pipeline illustrates the individual steps taken during the analysis of sequencing depth and evenness of a set of 194 genome records of complete plastid genomes. The pipeline comprises of four parts: (i) read mapping, (ii) metadata extraction, (iii) calculation of sequencing depth and evenness, and (iv) statistical analysis.
+
+
+Part i: Read mapping
+--------------------
+
+Input: _foo bar baz_
+
+##### Conducting read mapping on a series of plastid genome records
+```
+#!/bin/bash
+
+INPUT=/PATH/TO/samples_list.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read -r SRA SAMPLE ACCESSION;
+do
+bash ./read_mapping.sh $SAMPLE $ACCESSION $SRA </dev/null
+done < $INPUT
+IFS=$OLDIFS
+```
+
+Part ii: Metadata extraction
+----------------------------
+
+Input: _foo bar baz_
+
+##### Foo Bar Baz
+```
+foo bar baz
+```
+
+
+Part iii: Sequencing depth and evenness
+---------------------------------------
+
+Input: _foo bar baz_
+
+##### Foo Bar Baz
+```
+Rscript plastid_analysis.R
+```
+
+
+Part iv: Statistical analysis
+-----------------------------
+
+Input: _foo bar baz_
+
+##### Foo Bar Baz
+```
+foo bar baz
+```
--- code/read_mapping.sh
+++ code/read_mapping.sh
@@ -25,7 +25,7 @@ ACCESSION=$2
 SRA=$3
 
 # FOLDERS
-mkdir -p $SAMPLELOC/$SAMPLE/Backmapping
+mkdir -p $SAMPLELOC/$SAMPLE/ReadMapping
 
 # download GenBank
 ${NCBIENT}esearch -db nucleotide -query $ACCESSION | ${NCBIENT}efetch -format gb > $SAMPLELOC/$SAMPLE/${ACCESSION}.gb
@@ -37,23 +37,23 @@ ${NCBIENT}esearch -db nucleotide -query $ACCESSION | ${NCBIENT}efetch -format fa
 cd $SAMPLELOC/$SAMPLE
 ${SRAT}prefetch --max-size 50000000 $SRA
 cd /scratch/nilsj24/
-${SRAT}fasterq-dump.2.10.8 --split-3 --skip-technical $SAMPLELOC/$SAMPLE/$SRA/$SRA.sra -O $SAMPLELOC/$SAMPLE/Backmapping
+${SRAT}fasterq-dump.2.10.8 --split-3 --skip-technical $SAMPLELOC/$SAMPLE/$SRA/$SRA.sra -O $SAMPLELOC/$SAMPLE/ReadMapping
 
-java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE $SAMPLELOC/$SAMPLE/Backmapping/${SRA}.sra_1.fastq $SAMPLELOC/$SAMPLE/Backmapping/${SRA}.sra_2.fastq  $SAMPLELOC/$SAMPLE/Backmapping/${SRA}_1_PE.fastq $SAMPLELOC/$SAMPLE/Backmapping/${SRA}_1_SE.fastq $SAMPLELOC/$SAMPLE/Backmapping/${SRA}_2_PE.fastq $SAMPLELOC/$SAMPLE/Backmapping/${SRA}_2_SE.fastq ILLUMINACLIP:/$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
+java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar PE $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}.sra_1.fastq $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}.sra_2.fastq  $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_1_PE.fastq $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_1_SE.fastq $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_2_PE.fastq $SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_2_SE.fastq ILLUMINACLIP:/$EBROOTTRIMMOMATIC/adapters/TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
 
-# conduct backmapping
+# conduct mapping of reads
 REF=$SAMPLELOC/$SAMPLE/${ACCESSION}_ref.fasta
-READSPER1=$SAMPLELOC/$SAMPLE/Backmapping/${SRA}_1_PE.fastq
-READSPER2=$SAMPLELOC/$SAMPLE/Backmapping/${SRA}_2_PE.fastq
+READSPER1=$SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_1_PE.fastq
+READSPER2=$SAMPLELOC/$SAMPLE/ReadMapping/${SRA}_2_PE.fastq
 
-mkdir -p $SAMPLELOC/$SAMPLE/Backmapping/db
-${BOWTIE}bowtie2-build $REF $SAMPLELOC/$SAMPLE/Backmapping/db/$ACCESSION
-${BOWTIE}bowtie2 -x $SAMPLELOC/$SAMPLE/Backmapping/db/$ACCESSION -1  $READSPER1 -2 $READSPER2 -S $SAMPLELOC/$SAMPLE/Backmapping/${ACCESSION}_mapping.sam
-${SAM}samtools view -Sb -F 0x04 $SAMPLELOC/$SAMPLE/Backmapping/${ACCESSION}_mapping.sam > $SAMPLELOC/$SAMPLE/Backmapping/${ACCESSION}_mapping_OneMoreLocations.bam
-${SAM}samtools sort $SAMPLELOC/$SAMPLE/Backmapping/${ACCESSION}_mapping_OneMoreLocations.bam > $SAMPLELOC/$SAMPLE/${ACCESSION}_mapping_OneMoreLocations.sorted.bam
+mkdir -p $SAMPLELOC/$SAMPLE/ReadMapping/db
+${BOWTIE}bowtie2-build $REF $SAMPLELOC/$SAMPLE/ReadMapping/db/$ACCESSION
+${BOWTIE}bowtie2 -x $SAMPLELOC/$SAMPLE/ReadMapping/db/$ACCESSION -1  $READSPER1 -2 $READSPER2 -S $SAMPLELOC/$SAMPLE/ReadMapping/${ACCESSION}_mapping.sam
+${SAM}samtools view -Sb -F 0x04 $SAMPLELOC/$SAMPLE/ReadMapping/${ACCESSION}_mapping.sam > $SAMPLELOC/$SAMPLE/ReadMapping/${ACCESSION}_mapping_OneMoreLocations.bam
+${SAM}samtools sort $SAMPLELOC/$SAMPLE/ReadMapping/${ACCESSION}_mapping_OneMoreLocations.bam > $SAMPLELOC/$SAMPLE/${ACCESSION}_mapping_OneMoreLocations.sorted.bam
 ${SAM}samtools index $SAMPLELOC/$SAMPLE/${ACCESSION}_mapping_OneMoreLocations.sorted.bam
 
-rm -rf $SAMPLELOC/$SAMPLE/Backmapping
+rm -rf $SAMPLELOC/$SAMPLE/ReadMapping
 rm -rf $SAMPLELOC/$SAMPLE/$SRA
 
 # conduct PACVr Rscript
